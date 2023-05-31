640a73a5496750ccd692f678086922cd6dde3c1e
BwtrIv <pitivnv@gmail.com>
2023-05-31 11:35:39 +0600

    First ajax project

---
+++ Desktop/RestAPI projects/buildingsAJAXproject/src/main/java/com/IvanovPA/buildingsAJAXproject/BuildingsAjaXprojectApplication.java
@@ -0,0 +1,13 @@
+package com.IvanovPA.buildingsAJAXproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingsAjaXprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildingsAjaXprojectApplication.class, args);
+	}
+
+}
---
+++ Desktop/RestAPI projects/buildingsAJAXproject/src/test/java/com/IvanovPA/buildingsAJAXproject/BuildingsAjaXprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IvanovPA.buildingsAJAXproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildingsAjaXprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
